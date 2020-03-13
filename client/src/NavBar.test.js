ad96d49faa150d540385c2b698074970ad71a62e
Michael Callahan <CallahanMichaelE@gmail.com>
2020-03-13 12:34:44 -0600

    adjusted testing for cards

--- client/src/NavBar.test.js
+++ client/src/NavBar.test.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import { render, fireEvent} from "@testing-library/react";
+import { render, fireEvent} from "@testing-library/react"; 
 import NavBar from './NavBar';
+
 
 
 test('button fires and can still be read',()=> {
 
-    const { getByText } = render(<NavBar /> )
+    const { getByText, findByText } = render(<NavBar /> )
 
     getByText(/Google Trends/i)
-    
+
     
     
     const dmButton = getByText(/dark mode/i)
     
     fireEvent.click(dmButton);
+    
     getByText(/Google Trends/i)
+
     
 
 })
